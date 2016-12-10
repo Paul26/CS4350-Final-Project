2a07d46a3896d58e8bc5e7e427891b585dedd04c
Jeff LeFevre <jeff.lefevre@gmail.com>
2016-12-10 03:52:12 +0000

    blog.js now has code that will query blog.php/latest and use the resulting JSON to populate the appropriate DOM elements.

--- html/js/blog.js
+++ html/js/blog.js
@@ -1,3 +1,14 @@
 $(document).on("pageload",function(){
-	$.get( "blog.php/latest" );
+	$.get( "blog.php/latest" )
+	  .done( function( data ) {
+		var title = data[0].title;
+		var body  = data[0].body;
+		var author = data[0].author;
+		var createdDate = data[0].createdDate;
+
+		$("#blog-title").text(title);
+		$("#blog-body").text(body);
+		$("#blog-author").text(author);
+		$("#blog-created-date").text(createdDate);
+	});
 });
